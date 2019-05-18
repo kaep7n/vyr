ba6addf0a18cfca79f97130ab5182be21e162214
Matthias Hillermeier <m.hillermeier@adestis.de>
2019-05-18 16:39:00 +0200

    moved subscriber to file

--- src/Vyr.Playground.Grpc.Client/Program.cs
+++ src/Vyr.Playground.Grpc.Client/Program.cs
@@ -3,11 +3,10 @@ using Pubsub;
 using System;
 using System.Collections.Generic;
 using System.Threading;
-using System.Threading.Tasks;
 
 namespace Vyr.Playground.Grpc
 {
-    class Program
+    partial class Program
     {
         static void Main(string[] args)
         {
@@ -31,50 +30,6 @@ namespace Vyr.Playground.Grpc
             client.Subscribe();
 
             return client;
-        }
-
-        public class Subscriber
-        {
-            private readonly PubSub.PubSubClient pubSubClient;
-            private Subscription subscription;
-
-            public Subscriber(PubSub.PubSubClient pubSubClient)
-            {
-                this.pubSubClient = pubSubClient;
-            }
-
-            public void Subscribe()
-            {
-                this.subscription = new Subscription() { Id = Guid.NewGuid().ToString() };
-
-                Task.Run(async () =>
-                {
-                    using var call = this.pubSubClient.Subscribe(this.subscription);
-                
-                     while (true)
-                     {
-                         Console.WriteLine($"{this.subscription.Id}: waiting for data");
-
-                         if (!await call.ResponseStream.MoveNext())
-                         {
-                             Console.WriteLine($"{this.subscription.Id}: no data available");
-                             continue;
-                         }
-
-                         Console.WriteLine($"{this.subscription.Id}: Event received: " + call.ResponseStream.Current);
-                     }
-                 });
-            }
-
-            public void Publish()
-            {
-                this.pubSubClient.Publish(new Event { Topic = "config", Value = $"Message from {this.subscription.Id}" });
-            }
-
-            public void Unsubscribe()
-            {
-                this.pubSubClient.Unsubscribe(this.subscription);
-            }
         }
     }
 }
---
+++ src/Vyr.Playground.Grpc.Client/Subscriber.cs
@@ -0,0 +1,54 @@
+﻿using Pubsub;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Vyr.Playground.Grpc
+{
+    partial class Program
+    {
+        public class Subscriber
+        {
+            private readonly PubSub.PubSubClient pubSubClient;
+            private Subscription subscription;
+
+            public Subscriber(PubSub.PubSubClient pubSubClient)
+            {
+                this.pubSubClient = pubSubClient;
+            }
+
+            public void Subscribe()
+            {
+                this.subscription = new Subscription() { Id = Guid.NewGuid().ToString() };
+
+                Task.Run(async () =>
+                {
+                    using var call = this.pubSubClient.Subscribe(this.subscription);
+                
+                     while (true)
+                     {
+                         Console.WriteLine($"{this.subscription.Id}: waiting for data");
+
+                         if (!await call.ResponseStream.MoveNext())
+                         {
+                             Console.WriteLine($"{this.subscription.Id}: no data available");
+                             continue;
+                         }
+
+                         Console.WriteLine($"{this.subscription.Id}: Event received: " + call.ResponseStream.Current);
+                     }
+                 });
+            }
+
+            public void Publish()
+            {
+                this.pubSubClient.Publish(new Event { Topic = "config", Value = $"Message from {this.subscription.Id}" });
+            }
+
+            public void Unsubscribe()
+            {
+                this.pubSubClient.Unsubscribe(this.subscription);
+            }
+        }
+    }
+}
