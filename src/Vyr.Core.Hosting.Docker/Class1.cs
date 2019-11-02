1840fdf7fd03be168f29508ded9aa5c3cee818b8
Matthias Hillermeier <m.hillermeier@adestis.de>
2019-11-02 15:11:53 +0100

    docker tests

---
+++ src/Vyr.Core.Hosting.Docker/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Vyr.Core.Hosting.Docker
+{
+    public class Class1
+    {
+    }
+}
---
+++ src/Vyr.Playground.Docker/Program.cs
@@ -0,0 +1,25 @@
+﻿using Docker.DotNet;
+using Docker.DotNet.Models;
+using System;
+using System.Threading.Tasks;
+
+namespace Vyr.Playground.Docker
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var client = new DockerClientConfiguration(new Uri("npipe://./pipe/docker_engine"))
+                .CreateClient();
+
+            var containers = await client.Containers.ListContainersAsync(new ContainersListParameters() { All = true });
+
+            foreach (var container in containers)
+            {
+                Console.WriteLine($"{container.ID} {container.Status}");
+            }
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
